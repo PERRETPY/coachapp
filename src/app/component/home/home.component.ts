a653f31d16e497a856d8e9ecc76147371039bc53
perretpy <pierreyves.perret@gmail.com>
2022-05-06 17:08:27 +0200

    Fix infoMetaDonnees load on spreadsheet set

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -69,6 +69,7 @@ export class HomeComponent implements OnInit {
           console.log('Lets go');
           this.getWorkoutList();
           this.cd.detectChanges();
+          location.reload();
         }
       }
     );
