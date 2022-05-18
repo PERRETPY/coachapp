a3eb72ec5474dba5c7610d32bd58ce625e8e2811
Pierre-Yves Perret <pierreyves.perret@gmail.com>
2022-05-18 13:13:23 +0200

    Add days late on workout preview

--- src/app/component/workout/workout-preview/workout-preview.component.ts
+++ src/app/component/workout/workout-preview/workout-preview.component.ts
@@ -13,8 +13,39 @@ export class WorkoutPreviewComponent implements OnInit {
   @Input()
   traductionMap: Map<string, string>;
 
+  daysLate: number;
+
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.calculateLate();
+  }
+
+  calculateLate(): void {
+    const today = new Date();
+    let tabDate;
+    let daysGap;
+    if(this.workout.etat === 'non commencé') {
+      tabDate = this.workout.dateDebutPrevue.split('/');
+
+    }else if(this.workout.etat === 'en cours') {
+      tabDate = this.workout.dateFinPrevue.split('/');
+    }else {
+      return;
+    }
+
+    if(tabDate.length === 3) {
+      const englishDate = tabDate[1] + '/' + tabDate[0] + '/' + tabDate[2];
+      const dateCible = new Date(englishDate);
+
+      daysGap = Math.floor((today.getTime() - dateCible.getTime()) / 1000 / 60 / 60 / 24);
+
+      if(daysGap < 0) {
+        daysGap = 0;
+      }
+
+      this.daysLate = daysGap;
+    }
+  }
 
 }
